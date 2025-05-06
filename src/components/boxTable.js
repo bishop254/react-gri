c4d9c0a1c2d9cb0f291b882ce3463c8f058bfc9f
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2025-05-06 06:35:46 +0300

    feat: Added empty box component

---
+++ src/components/boxTable.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const BoxTableComponent = () => {
+  return (
+    <table
+      style={{
+        borderCollapse: "collapse",
+        width: "100%",
+      }}
+    >
+      <tr>
+        <td
+          style={{
+            border: "1px solid black",
+            height: "50px",
+          }}
+        >
+          &nbsp;
+        </td>
+      </tr>
+    </table>
+  );
+};
+
+export default BoxTableComponent;
